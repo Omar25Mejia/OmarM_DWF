1c94a4e612ecb746c602c99bffd7f5f97e9c7b5c
Antonio-9634 <diegoantonioffh@gmail.com>
2025-07-15 19:00:38 -0600

    Primer commit

---
+++ spring-ext-config/src/main/java/GM190258_semana4/spring_ext_config/SpringExtConfigApplication.java
@@ -0,0 +1,13 @@
+package GM190258_semana4.spring_ext_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExtConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExtConfigApplication.class, args);
+	}
+
+}
